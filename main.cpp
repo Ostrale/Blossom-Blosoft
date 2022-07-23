0434ae71b16d47bfcbe60580813dd9222f3207d6
Enzo Scafi <108572670+Ostrale@users.noreply.github.com>
2022-07-23 23:07:58 +0200

    Start project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+#define TINS_STATIC
+
 #include <iostream>
+#include <tins/arp.h>
+#include <tins/ip.h>
+#include <tins/tcp.h>
+#include <tins/ethernetII.h>
+
+using namespace Tins;
 
 int main() {
-    
-    return 0;
+    //do helo world
+    std::cout << "Hello, World!123" << std::endl;
+    EthernetII eth;
+    IP* ip = new IP();
+    TCP* tcp = new TCP();
+
+    // tcp is ip's inner pdu
+    ip->inner_pdu(tcp);
+
+    // ip is eth's inner pdu
+    eth.inner_pdu(ip);
 }
