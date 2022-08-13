f374795221b77431e5f938578ab6e8de0bc62648
Enzo Scafi <108572670+Ostrale@users.noreply.github.com>
2022-08-13 10:57:43 +0200

    First tests : reading the headers of IP protocoles not the goal but it's a good test

--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
-#define TINS_STATIC
-
+#include <tins/tins.h>
 #include <iostream>
-#include <tins/arp.h>
-#include <tins/ip.h>
-#include <tins/tcp.h>
-#include <tins/ethernetII.h>
-
+#include <string>
+#include <vector>
 using namespace Tins;
 
+struct renifleur{
+
+    int data_size = 0;
+
+    void renifle(){
+        // Create sniffer configuration object.
+        SnifferConfiguration config;
+        config.set_promisc_mode(true);
+
+        // Use the default interface
+        NetworkInterface iface = NetworkInterface::default_interface();
+
+        // print Default interface
+        std::cout << "Default interface name: " << iface.name();
+        std::wcout << " (" << iface.friendly_name() << ")" << std::endl;
+
+        // Now instantiate the sniffer
+        Sniffer sniffer(iface.name()); // Ready to sniff!
+        sniffer.sniff_loop(make_sniffer_handler(this, &renifleur::lecture));
+    }
+
+    bool lecture(PDU& pkt){
+        // Lookup the UDP PDU
+        const IP &ip = pkt.rfind_pdu<IP>();
+        int size_of_header = ip.header_size();
+        data_size += size_of_header;
+        std::cout << data_size << '\r' << std::flush ;
+        //boucle infini
+        return true;
+    }
+};
+
+
 int main() {
-    //do helo world
-    std::cout << "Hello, World!123" << std::endl;
-    EthernetII eth;
-    IP* ip = new IP();
-    TCP* tcp = new TCP();
-
-    // tcp is ip's inner pdu
-    ip->inner_pdu(tcp);
-
-    // ip is eth's inner pdu
-    eth.inner_pdu(ip);
+    renifleur r;
+    r.renifle();
+    return 0;
 }
