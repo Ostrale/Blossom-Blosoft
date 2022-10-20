920981c8122d20e3e917b8bb2da9349a0142bb8d
Enzo Scafi <108572670+Ostrale@users.noreply.github.com>
2022-10-20 23:07:22 +0200

    New chart test

---
+++ GUI/js/analytics.js
@@ -0,0 +1,29 @@
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(100, 60, 200)',
+        borderColor: 'rgb(50, 50, 150)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
