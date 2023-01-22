a754fc2f3de8faf74c4a26962d21506f6e0a09d0
Enzo Scafi <108572670+Ostrale@users.noreply.github.com>
2023-01-22 15:44:28 +0100

    Integrating nDPI library into the project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 /*
 Blosoft project
 Vert-y-good
-
 http://libtins.github.io/docs/latest/
 */
-
 #include <tins/tins.h>
+#include <ndpi_main.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,6 +13,16 @@ using namespace Tins;
 #include "toby.hpp"
 
 int main() {
+    //test nPDI npdi_main.h
+    ndpi_detection_module_struct *ndpi = ndpi_init_detection_module(ndpi_no_prefs);
+
+    if (ndpi != NULL) {
+        std::cout << "nDPI initialized successfully!" << std::endl;
+        ndpi_exit_detection_module(ndpi);
+    } else {
+        std::cout << "nDPI initialization failed!" << std::endl;
+    }
+    
     renifleur r;
     r.renifle();
     return 0;
--- toby.hpp
+++ toby.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <tins/tins.h>
+
 using namespace Tins;
 
 //fonction qui converti les octets en ko, mo, go en arrondissant à 2 chiffres après la virgule
@@ -12,6 +13,12 @@ std::string convertSize(double size);
 struct renifleur{
 
     int data_size = 0;
+    int streaming_size = 0;
+    int web_size = 0;
+    int mail_size = 0;
+    int cloud_size = 0;
+    int market_size = 0;
+    int other_size = 0;
 
     void renifle(){
         // Create sniffer configuration object.
@@ -26,26 +33,56 @@ struct renifleur{
         std::wcout << " (" << iface.friendly_name() << ")" << std::endl;
 
         // Now instantiate the sniffer
-        Sniffer sniffer(iface.name()); // Ready to sniff!
+        Sniffer sniffer(iface.name(),config);
         sniffer.sniff_loop(make_sniffer_handler(this, &renifleur::lecture));
     }
 
     bool lecture(PDU& pkt){
-        // Lookup the UDP PDU
-
-        if(pkt.find_pdu<IP>()){
+        // Lookup the IP PDU
+        if(pkt.find_pdu<IP>() && pkt.find_pdu<TCP>()){
             const IP &ip = pkt.rfind_pdu<IP>();
+            const TCP &tcp = pkt.rfind_pdu<TCP>();
             int size_of_packet_ip = ip.tot_len();
             data_size += size_of_packet_ip;
-        }else if(pkt.find_pdu<IPv6>()){
+            // Check the destination port
+            if (tcp.dport() == 80 || tcp.dport() == 8080 || tcp.dport() == 443 || tcp.dport() == 5222) { //vérif 5222
+                // Lookup the payload of the packet
+                const RawPDU &raw = pkt.rfind_pdu<RawPDU>();
+                // Get a string representation of the payload
+                std::string payload = std::string(raw.payload().begin(), raw.payload().end());
+                // Check if the payload starts with "GET" or "POST"
+                if (payload.find("GET") == 0 || payload.find("POST") == 0) { // a comprendre #TODO 
+                    web_size += size_of_packet_ip;
+                } else {
+                    other_size += size_of_packet_ip;
+            }
+            } else if (tcp.dport() == 25) {
+                mail_size += size_of_packet_ip;
+            } else {
+                other_size += size_of_packet_ip;
+            }
+        }else if(pkt.find_pdu<IPv6>() && pkt.find_pdu<TCP>()){
             const IPv6 &ipv6 = pkt.rfind_pdu<IPv6>();
+            const TCP &tcp = pkt.rfind_pdu<TCP>();
             int size_of_packet_ipv6 = ipv6.payload_length();
             data_size += size_of_packet_ipv6;
+            // Check the destination port
+            if (tcp.dport() == 80 || tcp.dport() == 8080 || tcp.dport() == 443 || tcp.dport() == 5222) { //vérif 5222
+                web_size += size_of_packet_ipv6;
+            } else if (tcp.dport() == 25) {
+                mail_size += size_of_packet_ipv6;
+            } else {
+                other_size += size_of_packet_ipv6;
+            }
         }
-
-        std::cout << convertSize(data_size) << '\r' << std::flush ;
-
-        //std::cout << ip.tos() << std::endl ;
+        std::cout << "Data usage:" << convertSize(data_size) <<
+            "Web:" << convertSize(web_size) <<
+            "Mail:" << convertSize(mail_size) <<
+            "Streaming:" << convertSize(streaming_size) <<
+            "Cloud:" << convertSize(cloud_size) <<
+            "Market:" << convertSize(market_size) <<
+            "Other:" << convertSize(other_size) <<
+            '\r' << std::flush;
         //boucle infini
         return true;
     }
